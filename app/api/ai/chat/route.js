89ef24c86a71bd4dcd1767dc4a17eafe1fbb669e
Tyson Hichman <tysonhichman@gmail.com>
2025-02-23 22:24:33 -0500

    increasing max duration

--- app/api/ai/chat/route.js
+++ app/api/ai/chat/route.js
@@ -3,6 +3,8 @@ import { HumanMessage } from '@langchain/core/messages';
 import { mainWorkflow } from '@/app/ai/main/main-workflow';
 import { createClient } from '@/utils/supabase/server';
 
+export const maxDuration = 300
+
 export async function POST(req) {
     try {
         const { query } = await req.json();
